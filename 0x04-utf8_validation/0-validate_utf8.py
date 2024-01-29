33115b0d681999b2ffb17dff91262d7effcfa043
BradleyGilden <bradleygilden@gmail.com>
2024-01-29 14:59:30 +0200

    utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Author: Bradley Dillion Gilden
@@ -6,4 +6,49 @@ Date: 29-01-2024
 """
 
 
+def validUTF8(data):
+    """checks if int array is valid utf8, where each int represents a byte"""
+    mask = 0xff
+    # code point
+    cp = 0
+
+    for b in data:
+        # prioritize lowest byte
+        b = b & mask
+        # ensure theres no code point greater than the 4byte like 1111 1---
+        if (b & 0xf8 == 0xf8):
+            return False
+        # if showing 10-- ---- placeholder but no codepoint was set
+        elif ((b & 0xc0 == 0x80) and (not cp)):
+            return False
+        # if showing 1,2,3 or 4 byte code point but the previous was not met
+        elif (((b & 0xc0 == 0xc0) or (b & 0xc8 == 0x00)) and cp):
+            return False
+        # set code point indicator to 4 bytes 1111 0---
+        elif (b & 0xf8 == 0xf0):
+            cp = 3
+        elif (b & 0xf0 == 0xe0):
+            cp = 2
+        elif (b & 0xe0 == 0xc0):
+            cp = 1
+        elif (b & 0xc8 == 0x00):
+            cp = 0
+
+        if ((b & 0xc0 == 0x80) and cp):
+            cp -= 1
+    # if there are leftover places that were allocated
+    if (cp):
+        return False
+    return True
+
+
 if __name__ == '__main__':
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32,
+            105, 115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))

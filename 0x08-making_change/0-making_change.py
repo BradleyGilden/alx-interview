48b7cd26d316a2a72612e75efc1c369793f4f52b
BradleyGilden <bradleygilden@gmail.com>
2024-02-27 12:42:39 +0200

    multiple versions

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,21 +8,13 @@ Date: 27-02-2024
 def makeChange(coins, total):
     """ find mininum number of coin values that sum to the total
     """
-    if (total == 0):
+    if total <= 0:
         return 0
-    maxval = total + 1
-    # build the array to hold number of coin values for each coin
-    dp = [maxval] * (maxval) 
-    # we know 0 coins satisfy a value of 0
-    dp[0] = 0
-    # for all amounts up until the desired amount calculate the number
-    # of coins required
-    for a in range(1, maxval):
-        for c in coins:
-            if a - c >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - c])
-
-    return dp[total] if dp[total] != maxval else -1
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
 
 
 if __name__ == '__main__':

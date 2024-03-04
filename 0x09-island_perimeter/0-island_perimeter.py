560dcc32b2311d23bc95aa55b471236b1336e9e3
BradleyGilden <bradleygilden@gmail.com>
2024-03-04 10:14:16 +0200

    test alter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -28,9 +28,9 @@ def island_perimeter(grid):
 
 if __name__ == "__main__":
     grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0],
     ]
     print(island_perimeter(grid))
     grid = [

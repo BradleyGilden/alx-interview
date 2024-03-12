9771ef111f483b1e916a04530b52bf1270a40689
BradleyGilden <bradleygilden@gmail.com>
2024-03-12 16:16:34 +0200

    one method solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,67 +5,25 @@ Prime Game
 
 
 def isWinner(x, nums):
-    """ wrapper for the inner functions """
+    """Determines winner of the prime number game"""
+    if x < 1 or not nums:
+        return None
 
-    def findMultiples(num, targets):
-        """
-        Finds multiples of a given number within a list
-        """
-        for i in targets:
-            if i % num == 0:
-                targets.remove(i)
-        return targets
+    max_n = max(nums)
+    primes = [True] * (max_n + 1)
+    primes[0], primes[1] = False, False
 
-    def isPrime(i):
-        """
-        Check if a number is prime.
-        """
-        if i == 1:
-            return False
-        for j in range(2, i):
-            if i % j == 0:
-                return False
-        return True
+    for i in range(2, int(max_n**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, max_n + 1, i):
+                primes[j] = False
 
-    def findPrimes(n):
-        """
-        Dispatch a given set into prime numbers and non-prime numbers.
-        """
-        counter = 0
-        target = list(n)
-        for i in range(1, len(target) + 1):
-            if isPrime(i):
-                counter += 1
-                target.remove(i)
-                target = findMultiples(i, target)
-            else:
-                pass
-        return counter
+    marias_wins, bens_wins = 0, 0
+    for n in nums:
+        primes_count = sum(primes[:n+1])
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
 
-    def getWinner(x, nums):
-        """ finds the winner
-        """
-        players = {'Maria': 0, 'Ben': 0}
-        cluster = set()
-        for elem in range(x):
-            nums.sort()
-            num = nums[elem]
-            for i in range(1, num + 1):
-                cluster.add(i)
-                if i == num + 1:
-                    break
-            temp = findPrimes(cluster)
-
-            if temp % 2 == 0:
-                players['Ben'] += 1
-            elif temp % 2 != 0:
-                players['Maria'] += 1
-
-        if players['Maria'] > players['Ben']:
-            return 'Maria'
-        elif players['Maria'] < players['Ben']:
-            return 'Ben'
-        else:
-            return None
-
-    return getWinner(x, nums)
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
